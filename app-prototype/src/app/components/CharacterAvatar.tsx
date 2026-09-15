fc161170caf5f9999f58bced3bbdad6cd949fbaa
Raditya Dahal <dahalraditya5511@gmail.com>
2026-09-15 08:09:52 +0300

    Change in story image, loading_page

--- app-prototype/src/app/components/CharacterAvatar.tsx
+++ app-prototype/src/app/components/CharacterAvatar.tsx
@@ -2,6 +2,7 @@ import nuppuBunny from '../../assets/png/NUPPU BUNNY.png';
 import muruBear from '../../assets/png/MURU BEAR.png';
 import hippuCat from '../../assets/png/HIPPU CAT.png';
 import lumoFox from '../../assets/png/LUMO FOX.png';
+import nuppuMark from '../../assets/png/NUPPU MARK.png';
 
 export type Species = 'bunny' | 'bear' | 'cat' | 'fox';
 
@@ -15,10 +16,13 @@ const ART: Record<Species, string> = {
 export function CharacterAvatar({
   species,
   className = '',
+  realArt = false,
 }: {
   species: Species;
   className?: string;
   wheelchair?: boolean;
+  realArt?: boolean;
 }) {
-  return <img src={ART[species]} alt={species} className={`${className} object-contain`} />;
+  // Showing the Nuppu mark instead of ART[species] for now, until the character art is ready everywhere.
+  return <img src={realArt ? ART[species] : nuppuMark} alt={species} className={`${className} object-contain`} />;
 }
